b644a674fa5519d6c002ccc8e20fd74c8353b84b
morteza2323 <morteza.abdolahi2323@gmail.com>
2026-09-22 03:03:17 +0330

    adding Open Graph Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,37 @@ import { SiteHeader } from "@/components/site-header";
 import { Providers } from "@/components/providers";
 
 export const metadata: Metadata = {
-  title: { default: "Robotsaz | Built for what is next", template: "%s | Robotsaz" },
-  description: "Industrial fabrication and additive manufacturing for complex ideas.",
-  icons:{icon:'/site-logo.PNG'}
+  title: {
+    default: "Robotsaz | Built for what is next",
+    template: "%s | Robotsaz",
+  },
+  description:
+    "Industrial fabrication and additive manufacturing for complex ideas.",
+  icons: { icon: "/site-logo.PNG" },
+  openGraph: {
+    title: "Robotsaz | Robotics & Engineering",
+    description:
+      "Robotics, industrial problem-solving, additive manufacturing and engineering solutions.",
+
+    url: "https://www.robotsaz.com",
+    siteName: "Robotsaz",
+
+    images: [
+      {
+        url: "/og-image.PNG",
+        width: 1200,
+        height: 630,
+        alt: "Robotsaz - Robotics and Engineering",
+      },
+    ],
+
+    type: "website",
+  },
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>
